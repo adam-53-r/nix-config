48a18f2d1b6331868297a5308451035fa5144bb1
Adam Rkouni <adam-53-r@protonmail.com>
2025-02-23 21:56:47 +0100

    Added JetBrains persistence

--- home/adamr/features/desktop/common/jetbrains.nix
+++ home/adamr/features/desktop/common/jetbrains.nix
@@ -1,7 +1,11 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
   home.packages = with pkgs; [
     jetbrains.clion
     jetbrains.rust-rover
     jetbrains.webstorm
+  ];
+
+  home.persistence."/persist/${config.home.homeDirectory}".directories = [
+    ".config/JetBrains"
   ];
 }
