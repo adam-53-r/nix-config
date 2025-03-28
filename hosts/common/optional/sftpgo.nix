8d1b3104817676e2c91def4a9b43bcf8f6590076
Adam Rkouni <adam-53-r@protonmail.com>
2025-03-29 00:43:02 +0100

    Added sftpgo config for regenerating ssl cert

--- hosts/common/optional/sftpgo.nix
+++ hosts/common/optional/sftpgo.nix
@@ -1,28 +1,45 @@
 {
+  config,
+  pkgs,
   ...  
-}: {
+}: let 
+  certificate_file = "${config.services.sftpgo.dataDir}/certificate.pem";
+  certificate_key_file = "${config.services.sftpgo.dataDir}/privatekey.pem";
+in {
   services.sftpgo = {
     enable = true;
     settings = {
+      data_provider = {
+        users_base_dir = "/var/lib/sftpgo/data";
+      };
       httpd.bindings = [
         {
           port = 8080;
-          address = "0.0.0.0";
           enable_web_client = true;
           enable_web_admin = true;
           enable_https = true;
-          certificate_file = "/var/lib/sftpgo/certificate.pem";
-          certificate_key_file = "/var/lib/sftpgo/privatekey.pem";
+          inherit certificate_file certificate_key_file;
         }
       ];
-      # ftpd.bindings = [
-      #   {
-      #     port = 21;
-      #     address = "0.0.0.0";
-      #   }
-      # ];
+      sftpd.bindings = [
+        {
+          port = 2022;
+        }
+      ];
     };
   };
+
+  # Generate self-signed SSL certificate in case there isn't one present
+  systemd.services.sftpgo.preStart = ''
+    # Make sure we don't write to stdout, since in case of
+    # socket activation, it goes to the remote side (#19589).
+    exec >&2
+    if !([ -e ${certificate_file} ] && [ -e ${certificate_key_file} ]); then
+      rm -f ${certificate_file} ${certificate_key_file}
+      ${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:4096 -keyout ${certificate_key_file} -out ${certificate_file} -sha256 -days 3650 -nodes -subj "/C=ES/CN=msi-server"
+    fi
+  '';
+
   environment.persistence = {
     "/persist" = {
       directories = [
