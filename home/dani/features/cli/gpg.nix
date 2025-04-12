64d0da55d263bbad67d4c171394de54a681e161c
Daniel Floria Lopez <dani-f53@protonmail.com>
2025-04-12 13:52:46 +0200

    Activated gpg agent

--- home/dani/features/cli/gpg.nix
+++ home/dani/features/cli/gpg.nix
@@ -4,15 +4,15 @@
   lib,
   ...
 }: {
-  # services.gpg-agent = {
-  #   enable = true;
-  #   enableSshSupport = true;
-  #   enableExtraSocket = true;
-  #   pinentryPackage =
-  #     if config.gtk.enable
-  #     then pkgs.pinentry-gnome3
-  #     else pkgs.pinentry-tty;
-  # };
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    enableExtraSocket = true;
+    pinentryPackage =
+      if config.gtk.enable
+      then pkgs.pinentry-gnome3
+      else pkgs.pinentry-tty;
+  };
 
   home.packages = lib.optional config.gtk.enable pkgs.gcr;
 
