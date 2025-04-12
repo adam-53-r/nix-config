08db4b811bfc92ff84997dcaccb12016de048247
Daniel Floria Lopez <dani-f53@protonmail.com>
2025-04-12 13:51:48 +0200

    Changed background wallpaper

--- home/dani/danix.nix
+++ home/dani/danix.nix
@@ -1,10 +1,22 @@
  {
   pkgs,
+  lib,
   ...
-}: {
+}: let 
+  artwork-pkg = builtins.fetchGit {
+    url = "https://github.com/NixOS/nixos-artwork";
+    rev = "33856d7837cb8ba76c4fc9e26f91a659066ee31f";
+  };
+in {
   imports = [
     ./global
     ./features/desktop/cinnamon
     ./features/games
   ];
+
+
+  dconf.settings = {
+    "org/cinnamon/desktop/background".picture-uri = "file://${artwork-pkg}/wallpapers/nix-wallpaper-binary-white.png";
+  };
+
 }
