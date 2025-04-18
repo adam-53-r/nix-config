8bebf989f959d40e63546e1601c9c6e864f81c7c
Adam Rkouni <adam-53-r@protonmail.com>
2025-04-18 16:28:15 +0200

    Added hyprland cachix server

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -7,8 +7,8 @@
 in {
   nix = {
     settings = {
-      # extra-substituters = lib.mkAfter ["https://cache.m7.rs"];
-      # extra-trusted-public-keys = ["cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="];
+      extra-substituters = lib.mkAfter ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
       trusted-users = [
         "root"
         "@wheel"
