9a61bbb97fd13ed4db5ec49fdf00970b81b55203
Adam Rkouni <adam-53-r@protonmail.com>
2025-04-20 10:54:57 +0200

    Added brave persistance

--- home/adamr/features/desktop/common/brave.nix
+++ home/adamr/features/desktop/common/brave.nix
@@ -1,3 +1,14 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
   home.packages = with pkgs; [brave];
+  home = {
+    persistence = {
+      # Not persisting is safer
+      "/persist/${config.home.homeDirectory}".directories = [
+        {
+          directory = ".config/BraveSoftware";
+          method = "bindfs";
+        }
+      ];
+    };
+  };
 }
