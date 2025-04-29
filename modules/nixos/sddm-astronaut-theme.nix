a6b706b890b512f45c155a94c13fa02a97085aa9
Adam Rkouni <adam-53-r@protonmail.com>
2025-04-29 12:56:09 +0200

    Added comment and config to sddm-astronaut module

--- modules/nixos/sddm-astronaut-theme.nix
+++ modules/nixos/sddm-astronaut-theme.nix
@@ -9,40 +9,6 @@ with lib; let
 in {
   options.services.displayManager.sddm.astronaut-theme = {
     enable = mkEnableOption "Sddm astronaut theme";
-
-    # steamcmdPackage = mkOption {
-    #   type = types.package;
-    #   default = pkgs.steamcmd;
-    #   defaultText = "pkgs.steamcmd";
-    #   description = ''
-    #     The package implementing SteamCMD
-    #   '';
-    # };
-
-    # dataDir = mkOption {
-    #   type = types.path;
-    #   description = "Directory to store game server";
-    #   default = "/var/lib/satisfactory";
-    # };
-
-    # launchOptions = mkOption {
-    #   type = types.str;
-    #   description = "Launch options to use.";
-    #   default = "";
-    # };
-
-    # openFirewall = mkOption {
-    #   type = types.bool;
-    #   default = false;
-    #   description = ''
-    #     Whether to open ports in the firewall for the server
-    #   '';
-    # };
-    config = let
-      configs = map (c: builtins.baseNameOf c) (lib.filesystem.listFilesRecursive "${pkgs.sddm-astronaut}/share/sddm/themes/sddm-astronaut-theme/Themes");
-    in mkOption {
-      default = builtins.head configs;
-    };
     config = let
       # Getting the possible configs for the theme by just listing the files in the Themes folder
       configs = map (c: builtins.baseNameOf c) (lib.filesystem.listFilesRecursive "${pkgs.sddm-astronaut}/share/sddm/themes/sddm-astronaut-theme/Themes");
@@ -53,42 +19,15 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # systemd.services.satisfactory-server = let
-    #   steamcmd = "${cfg.steamcmdPackage}/bin/steamcmd";
-    #   steam-run = "${pkgs.steam-run}/bin/steam-run";
-    # in {
-    #   description = "Satisfactory Dedicated Server";
-    #   wantedBy = ["multi-user.target"];
-    #   after = ["network.target"];
-
-    #   serviceConfig = {
-    #     TimeoutSec = "15min";
-    #     ExecStart = "${steam-run} ${cfg.dataDir}/FactoryServer.sh ${cfg.launchOptions}";
-    #     Restart = "always";
-    #     User = "satisfactory";
-    #     WorkingDirectory = cfg.dataDir;
-    #   };
-
-    #   preStart = ''
-    #     ${steamcmd} +force_install_dir "${cfg.dataDir}" +login anonymous +app_update 1690800 validate +quit
-    #   '';
-    # };
-
-    # users.users.satisfactory = {
-    #   description = "Satisfactory server service user";
-    #   home = cfg.dataDir;
-    #   createHome = true;
-    #   isSystemUser = true;
-    #   group = "satisfactory";
-    # };
-    # users.groups.satisfactory = {};
-
-    # networking.firewall = mkIf cfg.openFirewall {
-    #   allowedUDPPorts = [
-    #     15777
-    #     7777
-    #     15000
-    #   ];
-    # };
+    services.displayManager.sddm.theme = "sddm-astronaut-theme";
+    environment.systemPackages = with pkgs; [
+      (sddm-astronaut.overrideAttrs (oldAttrs: {
+        installPhase = let 
+          basePath = "$out/share/sddm/themes/sddm-astronaut-theme";
+        in oldAttrs.installPhase + ''
+          ${pkgs.sd}/bin/sd 'ConfigFile=Themes/astronaut.conf' 'ConfigFile=Themes/${cfg.config}' ${basePath}/metadata.desktop;
+        '';
+      }))
+    ];
   };
 }
