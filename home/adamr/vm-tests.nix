68cbfd5312585ed0e7f535df7ebf69fc453b2943
Adam Rkouni <adam-53-r@protonmail.com>
2025-05-20 15:14:29 +0200

    updated tests vm config

--- home/adamr/vm-tests.nix
+++ home/adamr/vm-tests.nix
@@ -1,8 +1,25 @@
- {
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./global
+    ./features/desktop/cinnamon
+    ./features/desktop/hyprland
+    ./features/games
+    ./features/productivity
+    ./features/pass
   ];
+
+  # Red
+  wallpaper = pkgs.inputs.themes.wallpapers.kosmos-space-dark;
+
+  monitors = [
+    {
+      name = "Virtual-1";
+      width = 1920;
+      height = 1080;
+      workspace = "1";
+      refreshRate = 60;
+      primary = true;
+    }
+  ];
+
 }
--- hosts/vm-tests/default.nix
+++ hosts/vm-tests/default.nix
@@ -1,11 +1,10 @@
 {
-  pkgs,
-  inputs,
-  lib,
   config,
-  options,
+  inputs,
+  pkgs,
+  lib,
   ...
-}: with lib; {
+}: {
   imports = [
     ./hardware-configuration.nix
 
@@ -13,8 +12,22 @@
     ../common/users/adamr
 
     ../common/optional/quietboot.nix
-    ../common/optional/mysql.nix
-    # ../common/optional/redis.nix
+    ../common/optional/sddm.nix
+    # ../common/optional/greetd.nix
+    ../common/optional/cinnamon.nix
+    ../common/optional/hyprland.nix
+    ../common/optional/pipewire.nix
+    ../common/optional/tlp.nix
+    ../common/optional/cups.nix
+    ../common/optional/wireshark.nix
+    ../common/optional/x11-no-suspend.nix
+    ../common/optional/gns3-client.nix
+    # ../common/optional/steam.nix
+    # ../common/optional/libvirtd.nix
+    ../common/optional/ecryptfs.nix
+    ../common/optional/docker.nix
+    # ../common/optional/virtualbox.nix
+    # ../common/optional/vmware.nix
   ];
 
   networking = {
@@ -29,56 +42,14 @@
 
   powerManagement.powertop.enable = true;
   programs = {
+    # light.enable = true;
     adb.enable = true;
     dconf.enable = true;
     fish.enable = true;
   };
 
-  networking.firewall = {
-    allowedUDPPorts = [ 51821 ]; # Clients and peers can use the same port, see listenport
-  };
-  # Enable WireGuard
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg-tests = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.69.69.2/24" ];
-      listenPort = 51821; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
 
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
-      privateKeyFile = config.sops.secrets.wg-priv-key.path;
-
-      peers = [
-        {
-          name = "msi-nixos";
-          # Public key of the server (not a file path).
-          publicKey = "iXKNESRze59Kamc6xXRujBwc4Xq+kAHMZc1wSalxfC0=";
-          presharedKeyFile = config.sops.secrets.wg-priv-key.path;
-          # Forward all the traffic via VPN.
-          # allowedIPs = [ "0.0.0.0/0" ];
-          # Or forward only particular subnets
-          allowedIPs = [ "10.69.69.1/32" ];
-
-          # Set this to the server IP and port.
-          endpoint = "192.168.122.1:51821"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
-
-  sops.secrets = {
-    wg-priv-key = {
-      sopsFile = ./secrets.json;
-      # neededForUsers = true;
-    };
-  };
+  services.displayManager.defaultSession = "hyprland-uwsm";
 
   system.stateVersion = "25.05";
 }
