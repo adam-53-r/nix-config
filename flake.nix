55757cb0db484f9dbf8f4d510a956ee20075c7e0
Adam Rkouni <adam-53-r@protonmail.com>
2025-05-25 10:56:08 +0200

    Changed hydra config

--- flake.nix
+++ flake.nix
@@ -126,7 +126,7 @@
     overlays = import ./overlays {inherit inputs outputs pkgs;};
     hydraJobs = import ./hydra.nix {inherit inputs outputs;};
 
-    # packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
+    packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
     devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
     formatter = forEachSystem (pkgs: pkgs.alejandra);
 
@@ -292,46 +292,46 @@
     };
 
     # Importing custom packages and nixos-generate configs
-    packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;}) //
-    {
-      x86_64-linux = {
-        install-iso = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          format = "install-iso";
-          modules = [
-            {
-              users.users.nixos = {
-                initialHashedPassword = lib.mkForce "$y$j9T$tRAkzHi9kpFVhiUg21FIQ0$mkHVaqB1A/Seq4NfGnZaBswCQNWQ/8FWPrVKR5Qo7zD";
-                openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile ./home/adamr/ssh.pub);
-              };
+    # packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;}) //
+    # {
+    #   x86_64-linux = {
+    #     install-iso = nixos-generators.nixosGenerate {
+    #       system = "x86_64-linux";
+    #       format = "install-iso";
+    #       modules = [
+    #         {
+    #           users.users.nixos = {
+    #             initialHashedPassword = lib.mkForce "$y$j9T$tRAkzHi9kpFVhiUg21FIQ0$mkHVaqB1A/Seq4NfGnZaBswCQNWQ/8FWPrVKR5Qo7zD";
+    #             openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile ./home/adamr/ssh.pub);
+    #           };
 
-              programs = {
-                fish.enable = true;
-              };
-              security.pam.sshAgentAuth = {
-                enable = true;
-              };
-              services.openssh = {
-                enable = true;
-                hostKeys = [
-                  {
-                    path = "/etc/ssh/ssh_host_ed25519_key";
-                    type = "ed25519";
-                  }
-                ];
-              };
-              boot = {
-                initrd = {
-                  availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "nvme" "usbhid" "usb_storage" ];
-                  kernelModules = [ "kvm-intel" ];
-                };
-                kernelModules = [ "kvm-intel" ];
-              };
-            }
-          ];
-        };
-      };
-    };
+    #           programs = {
+    #             fish.enable = true;
+    #           };
+    #           security.pam.sshAgentAuth = {
+    #             enable = true;
+    #           };
+    #           services.openssh = {
+    #             enable = true;
+    #             hostKeys = [
+    #               {
+    #                 path = "/etc/ssh/ssh_host_ed25519_key";
+    #                 type = "ed25519";
+    #               }
+    #             ];
+    #           };
+    #           boot = {
+    #             initrd = {
+    #               availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "nvme" "usbhid" "usb_storage" ];
+    #               kernelModules = [ "kvm-intel" ];
+    #             };
+    #             kernelModules = [ "kvm-intel" ];
+    #           };
+    #         }
+    #       ];
+    #     };
+    #   };
+    # };
 
 
     # Deploy-rs configs
--- hosts/msi-server/services/hydra/machines.nix
+++ hosts/msi-server/services/hydra/machines.nix
@@ -47,7 +47,6 @@ in {
         systems = [
           "x86_64-linux"
           "i686-linux"
-          "aarch64-linux"
         ];
         maxJobs = 2;
         supportedFeatures = [
