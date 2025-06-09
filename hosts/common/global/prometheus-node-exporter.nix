78fb65440e83de357a744af258313ab0a7aafc07
Adam Rkouni <adam-53-r@protonmail.com>
2025-06-09 18:39:47 +0200

    Changed prometheus node exporter config

--- hosts/common/global/prometheus-node-exporter.nix
+++ hosts/common/global/prometheus-node-exporter.nix
@@ -3,7 +3,7 @@
     enable = true;
     enabledCollectors = ["systemd"];
   };
-  networking.firewall.interfaces."tailscale0" = {
+  networking.firewall.interfaces."${config.services.tailscale.interfaceName}" = {
     allowedTCPPorts = [config.services.prometheus.exporters.node.port];
   };
 }
