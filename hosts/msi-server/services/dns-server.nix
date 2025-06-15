4e64a3aadb984ec2809293f1f94bc8c64ed634bf
Adam Rkouni <adam-53-r@protonmail.com>
2025-06-15 23:44:33 +0200

    Still wont work

--- hosts/msi-server/services/dns-server.nix
+++ hosts/msi-server/services/dns-server.nix
@@ -1,23 +1,31 @@
 {pkgs, ...}: {
-  services.dnsmasq.enable = false;
   services.bind = {
     enable = true;
+    listenOn = ["100.86.227.101"];
+    ipv4Only = true;
+    extraOptions = ''
+      allow-recursion { any; };
+      allow-query { any; };
+      dnssec-validation no;
+    '';
+    cacheNetworks = [ "any" ];
     zones = {
       "arm53.xyz" = {
         master = true;
-        file = pkgs.writeText "arm53.xyz" ''
+        file = pkgs.writeText "arm53.xyz.db" ''
           $ORIGIN arm53.xyz.
-          $TTL    1h
-          @            IN      SOA     ns1 hostmaster (
-                                           1    ; Serial
-                                           3h   ; Refresh
-                                           1h   ; Retry
-                                           1w   ; Expire
-                                           1h)  ; Negative Cache TTL
-                          IN      NS     ns1
-                          
-          dash            IN      A      100.86.227.101
-          ns1             IN      A      100.86.227.101
+          $TTL 2h
+
+          @               SOA     ns1 hostmaster (
+                                          2018111111 ; Serial
+                                          8h         ; Refresh
+                                          30m        ; Retry
+                                          1w         ; Expire
+                                          1h )       ; Negative Cache TTL
+                          NS      ns1
+
+          dash            A       100.86.227.101
+          ns1             A       100.86.227.101
         '';
       };
     };
