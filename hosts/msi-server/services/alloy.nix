aa560d57b00131f6ca821301dad5e95014650dab
Adam Rkouni <adam-53-r@protonmail.com>
2025-07-05 11:59:19 +0200

    Added alloy nginx log collector

--- hosts/msi-server/services/alloy.nix
+++ hosts/msi-server/services/alloy.nix
@@ -1,6 +1,15 @@
 {
+  config,
+  ...
+}: let
+  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
   services.alloy = {
     enable = true;
   };
-  environment.etc."alloy/config.alloy".source = ./alloy.yml;
+  environment.etc."alloy/config.alloy".source = ./config.alloy;
+
+  systemd.services.alloy.serviceConfig.SupplementaryGroups = ifTheyExist [
+    "nginx"
+  ];
 }
