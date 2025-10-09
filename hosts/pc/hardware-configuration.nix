ba275207a73d9d831fa552f0ab268927a8ca3dfc
Adam Rkouni <adam-53-r@protonmail.com>
2025-10-09 22:55:27 +0200

    Changed cpu config to amd

--- hosts/pc/hardware-configuration.nix
+++ hosts/pc/hardware-configuration.nix
@@ -47,7 +47,7 @@
     }
   ];
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   nixpkgs.hostPlatform.system = "x86_64-linux";
 
   hardware.graphics.enable = true;
