d0a8875efeb6233663acfc293d04bb5c8c569864
Adam Rkouni <adam-53-r@protonmail.com>
2025-11-07 21:57:55 +0100

    Disabled unread mail icon in waybar

--- home/adamr/features/desktop/common/wayland-wm/waybar.nix
+++ home/adamr/features/desktop/common/wayland-wm/waybar.nix
@@ -217,33 +217,33 @@ in {
             '';
           };
         };
-        "custom/unread-mail" = {
-          interval = 5;
-          return-type = "json";
-          exec = mkScriptJson {
-            deps = [pkgs.findutils pkgs.procps];
-            script = ''
-              count=$(find ~/Mail/*/Inbox/new -type f | wc -l)
-              if pgrep mbsync &>/dev/null; then
-                status="syncing"
-              else
-                if [ "$count" == "0" ]; then
-                  status="read"
-                else
-                  status="unread"
-                fi
-              fi
-            '';
-            text = "$count";
-            alt = "$status";
-          };
-          format = "{icon}  ({})";
-          format-icons = {
-            "read" = "󰇯";
-            "unread" = "󰇮";
-            "syncing" = "󰁪";
-          };
-        };
+        # "custom/unread-mail" = {
+        #   interval = 5;
+        #   return-type = "json";
+        #   exec = mkScriptJson {
+        #     deps = [pkgs.findutils pkgs.procps];
+        #     script = ''
+        #       count=$(find ~/Mail/*/Inbox/new -type f | wc -l)
+        #       if pgrep mbsync &>/dev/null; then
+        #         status="syncing"
+        #       else
+        #         if [ "$count" == "0" ]; then
+        #           status="read"
+        #         else
+        #           status="unread"
+        #         fi
+        #       fi
+        #     '';
+        #     text = "$count";
+        #     alt = "$status";
+        #   };
+        #   format = "{icon}  ({})";
+        #   format-icons = {
+        #     "read" = "󰇯";
+        #     "unread" = "󰇮";
+        #     "syncing" = "󰁪";
+        #   };
+        # };
         "custom/next-event" = {
           interval = 10;
           return-type = "json";
