6236efb8880f030aac1122df1766c315a53257e9
Adam Rkouni <adam-53-r@protonmail.com>
2026-01-16 00:00:18 +0100

    Fixed pass secret service

--- home/adamr/features/pass/default.nix
+++ home/adamr/features/pass/default.nix
@@ -6,7 +6,7 @@
   programs.password-store = {
     enable = true;
     settings = {
-      PASSWORD_STORE_DIR = "$HOME/.password-store";
+      PASSWORD_STORE_DIR = ".password-store";
     };
     package = pkgs.pass.withExtensions (p: [
       p.pass-otp
