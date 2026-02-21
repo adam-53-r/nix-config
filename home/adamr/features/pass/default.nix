2f92faa2392792763803355a2e50a07783debbb3
Adam Rkouni <adam-53-r@protonmail.com>
2026-02-21 12:47:02 +0100

    Fixed pass store dir env var bug

--- home/adamr/features/pass/default.nix
+++ home/adamr/features/pass/default.nix
@@ -6,7 +6,7 @@
   programs.password-store = {
     enable = true;
     settings = {
-      PASSWORD_STORE_DIR = ".password-store";
+      PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
     };
     package = pkgs.pass.withExtensions (p: [
       p.pass-otp
