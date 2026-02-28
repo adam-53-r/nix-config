50a052f86a1e703010aa3fb1669914d5ec767fb7
Adam Rkouni <adam-53-r@protonmail.com>
2026-02-28 12:14:29 +0100

    Added syslog source to alloy logs ingester

--- hosts/msi-server/services/alloy.nix
+++ hosts/msi-server/services/alloy.nix
@@ -4,7 +4,11 @@ in {
   services.alloy = {
     enable = true;
   };
+
   environment.etc."alloy/config.alloy".source = ./config.alloy;
+
+  # Only allow Mikrotik to send syslog to alloy
+  networking.firewall.extraInputRules = "ip6 saddr { fd16:a5f8:258:2::1/128 } udp dport 51893 accept";
 
   systemd.services.alloy.serviceConfig.SupplementaryGroups = ifTheyExist [
     "nginx"
