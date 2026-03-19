3349957f710fd3b7057f1edb16daaa260f22dfaa
Adam Rkouni <adam-53-r@protonmail.com>
2026-03-19 20:32:43 +0100

    Disabled openrgb custom module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,7 @@
 {
   satisfactory = import ./satisfactory.nix;
   hydra-auto-upgrade = import ./hydra-auto-upgrade.nix;
-  openrgb = import ./openrgb.nix;
+  # openrgb = import ./openrgb.nix;
   sddm-astronaut-theme = import ./sddm-astronaut-theme.nix;
   disable-user-sops = import ./disable-user-sops.nix;
   hytale = import ./hytale.nix;
